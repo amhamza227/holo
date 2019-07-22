3b77a4bc07488eab00a4d4e1f8318e03626f0d85
Mohsan iqbal <mohsaniqbal387@gmail.com>
2019-07-23 00:35:57 +0500

    23-07-2019

--- src/app/registration-level1/registration-level1.page.ts
+++ src/app/registration-level1/registration-level1.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registration-level1',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationLevel1Page implements OnInit {
   isLinear = true;
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
+  }
+
+  register(){
+    this.router.navigateByUrl('registration-level2');
   }
 
 }
--- src/app/registration-level2/registration-level2.page.ts
+++ src/app/registration-level2/registration-level2.page.ts
@@ -1,4 +1,6 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registration-level2',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationLevel2Page implements OnInit {
 
-  constructor() { }
+  constructor(public alertController: AlertController, public router: Router) { }
 
   ngOnInit() {
+  }
+
+  register(){
+    this.presentAlertConfirm();
+  }
+
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      mode: 'ios',
+      header: 'Confirm!',
+      message: '<img src="./../../assets/imgs/person-wheel.png"><br>Messagetext</img>!!!',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            console.log('Confirm Okay');
+            this.router.navigateByUrl('registration-level3');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
 }
